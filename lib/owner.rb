87b46911bd9e0776e160a02db235e457b720a316
Kiri Um <kirirotha@gmail.com>
2020-07-21 18:49:46 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,36 @@
 class Owner
+  attr_reader :name, :species
+  @@all = []
+  def initialize(name, species = "human")
+    @species = species
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def self.count
+    @@all.count
+  end
+
+  def self.reset_all
+    @@all = []
+  end
+
+  def cats
+    Cat.all select do |c|
+      c.owner.name == self.name
+    end
+
+  end
+
+
+
   # code goes here
 end
---
+++ run.rb
@@ -0,0 +1,8 @@
+require 'pry'
+require_relative 'lib/dog.rb'
+require_relative 'lib/cat.rb'
+require_relative 'lib/owner.rb'
+
+
+
+binding.pry
